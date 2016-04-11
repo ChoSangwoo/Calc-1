cb4cd7c782f1e44f990ce8b52c9e78fcfcc02b26
multimedia <multimedia@309-29>
2016-04-11 16:38:12 +0900

    sub 추가

--- calculatorN/src/main/java/calculatorN/Calc.java
+++ calculatorN/src/main/java/calculatorN/Calc.java
@@ -1,5 +1,14 @@
 package calculatorN;
 
 public class Calc {
-
+   private int result = 0;
+   public void add(int x, int y){
+      result = x + y;
+   }
+   public void sub(int x, int y){
+      result = x- y;
+   }
+   public int getResult(){
+      return result;
+   }
 }
--- calculatorN/src/test/java/calculatorN/CalcTest.java
+++ calculatorN/src/test/java/calculatorN/CalcTest.java
@@ -1,5 +1,22 @@
 package calculatorN;
 
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
 public class CalcTest {
 
+   @Test
+   public void testAdd() {
+      Calc calc = new Calc();
+      calc.add(10, 20);
+      assertEquals(30,calc.getResult());
+   }
+   
+   @Test
+   public void testsub() {
+      Calc calc = new Calc();
+      calc.sub(10, 20);
+      assertEquals(-10,calc.getResult());
+   }
 }
